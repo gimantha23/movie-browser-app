2ec119b181b3bf175a4bcbbef229fc95d57da916
Gimantha De Silva <gimantha.k98@gmail.com>
2023-11-11 13:39:47 +0000

    added pages and routing

---
+++ src/Components/Favorites/Favorites.jsx
@@ -0,0 +1,6 @@
+const Favorites = () => {
+    return <h1>Favorites page</h1>;
+  };
+  
+  export default Favorites;
+  
---
+++ src/Components/MovieDetail/MovieDetail.jsx
@@ -0,0 +1,5 @@
+const MovieDetail = () => {
+    return <h1>Movie Detail page</h1>;
+  };
+  
+  export default MovieDetail;
